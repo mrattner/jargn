3949c06722f877747b4093ef1be22b12e6f5d9e3
Mayu Kudo <mayukud@gmail.com>
2013-03-04 17:11:43 -0500

    Mayu

--- app.js
+++ app.js
@@ -35,3 +35,114 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+
+
+/*
+ * Login.
+ */
+var PORT = 3000;
+var app  = require('express').createServer();
+app.set('view options', { layout : false });
+app.set('view engine',  'ejs');
+
+// MongoDB setting
+var mongoose = require('mongoose').Mongoose;
+
+// Model for MangoDB
+mongoose.model('User', {
+    properties : [ 'userid', 'password', 'created_at' ],
+    methods    : {
+        save   : function (fn) {
+            this.created_at = new Date();
+            this.__super__(fn);
+        }
+    }
+});
+
+// Access to database
+var db = mongoose.connect('mongodb://localhost:3000/mydb');
+
+
+// Login
+app.get("/", function (req, res) {
+    res.render('index', {
+        locals : { message : '' }
+    });
+});
+
+// Submit
+app.get("/login", function (req, res) {
+
+    var res_error = function (mes) {
+        res.render('index', {
+            locals : { message : mes }
+        });
+    };
+
+    var res_success = function (mes) {
+        res.render('success', {
+            locals : {
+                "message"  : mes,
+                "userid"   : userid,
+                "password" : password
+            }
+        });
+    };
+
+    var userid     = req.param('userid');
+    var password   = req.param('password');
+    var create_new = req.param('new');
+
+    // When userid is not valid
+    if (! userid) {
+        res_error('not valid "User ID"');
+    }
+    // When password is not valid
+    if (! password) {
+        res_error('not valid "password"');
+    }
+
+    var User  = db.model('User');
+    var user  = new User();
+    var i     = { "userid" : userid, "password" : password };
+
+    // Create a new account
+    if (create_new) {
+
+        User.find(i).one(function (doc) {
+            console.log(doc);
+            // No duplicated userid so accesses to database
+            if (doc == null) {
+                user.userid   = userid;
+                user.password = password;
+                user.save();
+
+                res_success('Welcome to Twitter');
+
+            }
+            // Duplicated userid
+            else {
+                res_error('choose different username');
+            }
+        });
+
+    }
+    // Login check
+    else {
+        User.find(i).one(function (doc) {
+            console.log(doc);
+            if (doc == null) {
+                res_error("UserID or passwrod is not valid.");
+            } else {
+                res_success('logged in');
+            }
+        });
+
+    }
+
+});
+
+
+app.listen(PORT);
+
