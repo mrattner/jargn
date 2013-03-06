fa6104cc0cb4b4880db0bbcc410b60c1efc15df6
Marcy Rattner <marcy.rattner@gmail.com>
2013-03-05 23:28:27 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-
-/*
- * GET home page.
+/**
+ * Home module: Show login page if user is not logged in. Show the home page once user has logged in.
  */
-
+ 
+//Render the index page
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Jargn' });
 };
