92d9eb5e382791fa55492840e6e70c3fc14f547c
Marcy Rattner <marcy.rattner@gmail.com>
2013-03-05 19:48:47 -0500

    Update myprofile.js

--- routes/myprofile.js
+++ routes/myprofile.js
@@ -1,3 +1,4 @@
 /**
- *  My Profile module: Represents the current user's profile.
+ *  My Profile module: Represents the current user's profile, including tweets and retweets, list of followers,
+ *  list of followed users, favorited tweets, lists, and requests.
  */
