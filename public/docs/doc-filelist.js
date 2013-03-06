db5fd1dbbb9b4d2d2888dbba568bc7bd38327cc8
Mayu Kudo <mayukud@gmail.com>
2013-03-06 10:52:23 -0500

    Mayu

--- public/docs/doc-filelist.js
+++ public/docs/doc-filelist.js
@@ -1,1 +1,1 @@
-var tree={"files":["connect.js","discover.js","findfriends.js","help.js","index.js","myprofile.js","numfollower.js","numfollowing.js","numtweet.js","search.js","settings.js","signup.js","tweet.js","user.js"]};
+var tree={"files":["connect.js","discover.js","findfriends.js","help.js","index.js","loginstatus.js","myprofile.js","numfollower.js","numfollowing.js","numtweet.js","search.js","settings.js","signup.js","tweet.js","user.js"]};
