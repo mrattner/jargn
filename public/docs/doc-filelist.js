906f50f4a6015e2c5d8da227eebf5f5b7be03a19
Marcy Rattner <marcy.rattner@gmail.com>
2013-03-11 17:14:10 -0400

    Generated up-to-date documentation

--- public/docs/doc-filelist.js
+++ public/docs/doc-filelist.js
@@ -1,1 +1,1 @@
-var tree={"files":["users.js"]};
+var tree={"files":["README.md","app.js"],"dirs":{"lib":{"files":["users.js"]},"public":{"dirs":{"javascripts":{"files":["forms.js"]}}},"routes":{"files":["connect.js","discover.js","findfriends.js","help.js","index.js","login.js","myprofile.js","search.js","settings.js","signup.js","tweet.js","user.js"]}}};
