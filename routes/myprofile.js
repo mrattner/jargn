acc40955984df6c711c77d3404002f4d35d1687f
Kira <kirarevere@gmail.com>
2013-03-11 21:30:55 -0400

    changed stuff with the tweet module

--- routes/myprofile.js
+++ routes/myprofile.js
@@ -1,9 +1,28 @@
 /**
- *  My Profile module: Represents the current user's profile, including tweets
+ *  Profile module: Represents a user's profile, including tweets
  *	and retweets, list of followers, list of followed users, favorited tweets,
  *	lists, and requests.
+ *	if it's the current users' profile then it displays as 'my profile'
+ *	otherwise it displays as another users respective profile
  */
+
+// post/tweet database
+var postlib = require('../lib/posts');
+var userlib = require('../lib/users');
+var loggedIn = require('./login').loggedIn;
+
+// ### *function*: display
+// Provides a view of a user's activity, his tweets,etc.
+
 exports.display = function (req, res) {
-	res.render('myprofile', {	title	: 'Jargn : My Profile',
-								user	: req.session.user});
+	if(loggedIn(req)){
+		res.render('profile', {	title	: 'Jargn : Profile' 
+														user	: req.session.user});
+	}
+	else{
+		req.flash('auth', message);
+		res.redirect('/login');
+	}
 }
+
+
