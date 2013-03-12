3bc6bd70cdc752e30df164dcc91889a677e82f1a
Kira <kirarevere@gmail.com>
2013-03-11 23:59:04 -0400

    fixed more conflicts

--- routes/myprofile.js
+++ routes/myprofile.js
@@ -6,19 +6,12 @@
  *	otherwise it displays as another users respective profile
  */
 
-<<<<<<< HEAD
 // post/tweet database
 var postlib = require('../lib/posts');
 var userlib = require('../lib/users');
 var loggedIn = require('./login').loggedIn;
 
-// ### *function*: display
-// Provides a view of a user's activity, his tweets,etc.
-
-=======
-// ### *function*: display
 // Provides a user his/her profile.
->>>>>>> 8bbe56b34acdbbf2967fd9d8c28f9fbee433cdaf
 exports.display = function (req, res) {
 	if(loggedIn(req)){
 		res.render('profile', {	title	: 'Jargn : Profile', 
