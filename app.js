c2a3935cbc31501a35746f0f19aeb17339c7edec
Marcy Rattner <marcy.rattner@gmail.com>
2013-04-08 04:04:49 -0400

    Implemented following basics
    Buggy and hacky. The user object that is stored in the session is not the same kind of user object that is stored
    in the user database, which caused some problems.
    Bug: 'Followers' and 'following' lists do not get updated correctly--user B has user A as both follower
    and following, but user B doesn't show up on either of user A's lists
    Unfinished: not allowing you to follow a person more than once

--- app.js
+++ app.js
@@ -49,12 +49,10 @@ function authmw (req, res, next) {
 		//If a logged out user is trying to log in or sign up
 		if (req.url.match(signupOrLoginURL) || req.url.match(stylesheetURL)) {
 			//Then continue to next route
-			console.log('continue (logged out) because url was ' + req.url);
 			next();
 		}
 		//Otherwise, redirect to login
 		else {
-			console.log('redirect (login) because url was ' + req.url);
 			login.display(req, res, next);
 		}
 	}
@@ -63,12 +61,10 @@ function authmw (req, res, next) {
 		//If a logged in user is trying to log in or sign up
 		if (req.url.match(signupOrLoginURL)) {
 			//Then redirect to index
-			console.log('redirect (index) because url was ' + req.url);
 			routes.index(req, res, next);
 		}
 		//Otherwise, continue to next route
 		else {
-			console.log('continue (logged in) because url was ' + req.url);
 			next();
 		}
 	}
@@ -138,6 +134,7 @@ app.get('/signup', signup.display);
 app.post('/signup/auth', signup.auth);
 app.get('/post', post.display);
 app.get('/user/:username', user.display);
+app.post('/user/:username/follow', user.followUser);
 
 // Web Sockets/Socket.IO
 var io	= require('socket.io', {'log level': 0}).listen(8000);
--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,26 @@
-
-/*
- * GET users listing.
- */
+// # User Profile Module
+// Represents a user of Jargn. Collects one person's posts, and shows
+// information about this person, who is following him/her, and other users
+// he/she is following.
 
 // Access the user database
 var userlib = require('../lib/users');
 // Access the post database
 var postlib = require('../lib/posts');
 
-// ### *function*: list
-// Gets user's listing.
-  exports.list = function(req, res){
-  res.send("respond with a resource");
-};
+//The user object whose profile this is
+var profileUser;
 
 // ### *function*: display
 // Renders the user's profile.
-exports.display = function(req, res){
-	var other = req.params.username;
-	console.log(other);
-	userlib.getUserByName(other, function (err, theUser){
-		if(err){
+exports.display = function (req, res){
+	//Get the user whose profile this is
+	userlib.getUserByName(req.params.username, function (err, theUser) {
+		if (err) {
 			res.send('User not found', 404);
 		}
-		else{
+		else {
+			profileUser = theUser;
 			var followers;
 			userlib.getFollowers(theUser, function(err, arr){
 				if(err){}
@@ -39,9 +36,8 @@ exports.display = function(req, res){
 				}
 			});
 			var posts;
-			postlib.getPostsByUser(theUser, function(err, arr){
-				if(err){}
-				else{
+			postlib.getPostsByUser(theUser, function (err, arr) {
+				if (!err) {
 					posts = arr;
 				}
 			});
@@ -50,7 +46,35 @@ exports.display = function(req, res){
 								 user		: req.session.user,
 	 							 ouFollowers: followers,
 	 							 ouFollowing: following,
-	 							 ouPosts	: posts});
+	 							 ouPosts	: posts,
+								 isFollowing: userlib.isFollowing});
+		}
+	});
+};
+
+// ### *function*: followUser
+// Adds the currently logged in user as a follower of this user's profile,
+// and adds this user to the currently logged in user's follow list.
+// @param req {object} The HTTP request
+// @param res {object} The HTTP response
+exports.followUser = function (req, res) {
+	console.log('started calling function');
+	//The currently logged in user
+	userlib.getUserByName(req.session.user.username, function (error, currUser) {
+		if (error) {
+			console.log('404 error');
+			res.send("The currently logged in user couldn't be found", 404);
+		} else {
+			console.log('check if profileUser is currUser');
+			//Current user cannot follow himself
+			if (profileUser !== currUser) {
+				console.log('trying to get currUser to follow profileUser');
+				userlib.follow(currUser, profileUser);
+				res.send('followed ' + profileUser.username, 200);
+			}
+			else{
+				res.send("you can't follow yourself!", 500);
+			}
 		}
 	});
 };
