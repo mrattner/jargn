23d54a6eef95d06db8e6e3ac145bde8063d5e0fb
Marcy Rattner <marcy.rattner@gmail.com>
2013-04-11 21:35:20 -0400

    Completed the compose post module

--- routes/post.js
+++ routes/post.js
@@ -1,26 +1,35 @@
-// # Post module: gets a composed post and adds to user's list of tweets
+// # Post module
+// Allows user to compose and upload a text post.
+
+// The posts library
+var postlib = require('../lib/posts');
 
 // ### *function*: display
-/**
-	*	provides a user post view
-	*
-	*	@param {object} req the HTTP request
-	*	@param {object} res the HTTP response
-	*/
-exports.display = function (req, res) {
-	res.render('post', {	title	:  'Jargn : Compose Post',	user	: req.session.user});
+// provides a user post view
+// @param req {object} the HTTP request
+// @param res {object} the HTTP response
+function display (req, res) {
+	res.render('post', {	title	:  'Jargn : Compose Post',
+							user	: req.session.user});
 }
+exports.display = display;
 
-// ## *function*: upload
-/**
-	* stores tweet
-	* @param {object} res the HTTP response
-	* @param {object} postData the text from the post
-	*/
-
-exports.upload = function (res, postData) {
-	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: " + postData);
-	response.end();
+// ### *function*: upload
+// Stores a post by the currently logged in user in the posts database.
+// @param req {object} the HTTP request
+// @param res {object} the HTTP response
+function upload (req, res) {
+	//Pull the author's username from the request
+	var author = req.session.user.username;
+	//Pull the text from the post request
+	var postText = req.body.post_text;
+	//Is this post private?
+	var isPrivate = (req.body.private) ? true : false;
+	
+	//Add the post to the posts database
+	postlib.addPost(postText, author, isPrivate);
+	
+	//Redirect to home page
+	res.redirect('/');
 }
+exports.upload = upload;
