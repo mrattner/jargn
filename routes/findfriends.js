e09b43244dafe5ac3bc314f07846102661c0b0f7
Marcy Rattner <marcy.rattner@gmail.com>
2013-04-11 21:37:19 -0400

    Deleted broken code in Find Friends module

--- routes/findfriends.js
+++ routes/findfriends.js
@@ -1,11 +1,7 @@
-/**
- *  Find friends module: plugs into email and social media APIs to retrieve
- *	contacts; fetches list of people you may know based on whom you already
- *	follow
- */
-
-//The user database
-var userlib = require('../lib/users');
+// # Find friends module
+// Plugs into email and social media APIs to retrieve
+// contacts; fetches list of people you may know based on whom you already
+// follow.
 
 // ### *function*: display
 // Provides a user Find Friends view.
@@ -15,30 +11,3 @@ exports.display = function (req, res) {
 								user	: req.session.user,
 								message : getUserMessage});
 }
-
-// ### *function*: getUser
-/**
- *	Performs basic getUser.
- *	
- *	@param {object} req The HTTP request
- *	@param {object} res The HTTP response
- */
-function getUser (req, res) {
-	//Pull the value from the input
-	var username = req.body.username;
-		
-	//Attempt to authenticate the user
-	userlib.getUserByName(username, function (error, message) {
-		if (error) {
-			//If there was an error, flash a message to the redirected route 'findfriends'
-			req.flash('getUser', error);
-			res.redirect('/findfriends');
-		}
-		else {
-			req.flash('getUser', message);
-			res.redirect('/findfriends');
-		}
-		
-	});
-}
-exports.getUser = getUser;
