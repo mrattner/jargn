f481f0941e51cf2eb2dd14fcf1eff724839cdade
Marcy Rattner <marcy.rattner@gmail.com>
2013-04-21 21:51:41 -0400

    Changed follow-client from example code
    Integrated client-side and server-side follow action

--- public/javascripts/follow-client.js
+++ public/javascripts/follow-client.js
@@ -1,21 +1,23 @@
-// # 
-// A ChatClient object for communicating
-// with the chat server.
-function ChatClient(config) {
+// # follow.client.js
+// Updates the client to show recent follow activity without requiring
+// page reloads.
+
+// A FollowClient object for communicating with the server.
+function FollowClient(config) {
 	for (var prop in config) {
 		this[prop] = config[prop];
 	}
 }
 
-ChatClient.prototype = {
-	// An cache of posts received from server.
-	posts : [],
+FollowClient.prototype = {
+	// A cache of follow activity received from server.
+	follows : [],
 	
 	// Start polling the server.
 	poll : function () {
 		var that = this;
 		this._stop = setInterval(function () {
-								 that.check();
+								 //that.check();
 								 },
 								 3000);
 	},
@@ -25,83 +27,60 @@ ChatClient.prototype = {
 		clearInterval(this._stop);
 	},
 	
-	// Post text to the server.
-	post : function (text) {
+	// Post follow activity to the server.
+	follow : function (followed) {
 		$.ajax({
-			   type : 'POST',
-			   url  : '/post',
-			   data : { 'text' : text },
-			   dataType : 'json'
-			   }).done(function (data) {
-					   console.log('Post status: ' + data.status);
-					   });
+		   type : 'POST',
+		   url  : '/user/:username/follow',
+		   data : {'followed' : followed},
+		   dataType : 'json'
+		   }).done(function (data) {
+				   console.log('Post status: ' + data.status);
+		   });
 	},
 	
-	// Check for more messages on the server
-	// given the last index we have for the
-	// current posts.
-	check : function () {
-		var that = this;		
-		$.ajax({
-			   type : 'POST',
-			   url  : '/check',
-			   data : { last : that.posts.length },
-			   dataType : 'json'
-			   }).done(function (data) {
-					   console.log('Check rcvd: ' + JSON.stringify(data));
-					   
-					   // Append the posts to the current posts:
-					   that.posts = that.posts.concat(data);
-					   
-					   // Rewrite to the view:
-					   that.view.empty();
-					   for (var i = 0; i < that.posts.length; i++) {
-					   var li   = $('<li>');
-					   var date = new Date(that.posts[i].date);
-					   li.html(date.toDateString() + ': ' + that.posts[i].text);
-					   that.view.append(li);
-					   }
-					   });
-	}	
-};
-
-function PostButton(config) {
-	for (var prop in config) {
-		this[prop] = config[prop];
-	}
-}
-
-PostButton.prototype = {
-	bind : function (type, cb) {
-		var that = this;
-		this.view.bind(type, function (event) {
-					   cb.call(that, event);
-					   });
-	}
-};
+//	// Check for more messages on the server given the last index we have for
+//	// thecurrent posts.
+//	check : function () {
+//		var that = this;		
+//		$.ajax({
+//		   type : 'POST',
+//		   url  : '/check',
+//		   data : { last : that.posts.length },
+//		   dataType : 'json'
+//		   }).done(function (data) {
+//			   console.log('Check rcvd: ' + JSON.stringify(data));
+//			   
+//			   // Append the posts to the current posts:
+//			   that.posts = that.posts.concat(data);
+//			   
+//			   // Rewrite to the view:
+//			   that.view.empty();
+//			   for (var i = 0; i < that.posts.length; i++) {
+//				var li   = $('<li>');
+//				var date = new Date(that.posts[i].date);
+//				li.html(date.toDateString() + ': ' + that.posts[i].text);
+//				that.view.append(li);
+//			   }
+//			});
+//	}	
+//};
 
 // jQuery ready handler:
-$(function () {
+$(document).ready(function () {
+				  console.log("The document is ready");
   // Get the list view that the chat client
   // will populate with incoming messages:	
-  var chatc = new ChatClient({ view : $('ul#chat') });
+  var followc = new FollowClient({ view : $('ol#followers') });
   
   // Start polling:
-  chatc.poll();
-  
-  // Setup the post button:
-  var postb = new PostButton({
-							 view  : $('button'),
-							 input : $('input')
-							 });
+  // followc.poll();
   
   // Bind a click event:
-  postb.bind('click', function (event) {
-			 console.log(this);
-			 var text = this.input.val();
-			 chatc.post(text);
-			 // clear input text:
-			 this.input.val('');
-			 return false;
-			 });
+  $('#followB').click(function (event) {
+					  console.log("button clicked");
+		followc.follow(this.attr('profileUser'));
+		//Bypass default page reload	
+		return false;
   });
+});
